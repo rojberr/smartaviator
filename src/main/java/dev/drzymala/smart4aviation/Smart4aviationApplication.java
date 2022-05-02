1f46440291452d38252cbc1de91e19b7df31cc20
Rojberr <rojberr@outlook.com>
2022-05-02 18:21:28 +0200

    feat(): Init commit

---
+++ src/main/java/dev/drzymala/smart4aviation/Smart4aviationApplication.java
@@ -0,0 +1,13 @@
+package dev.drzymala.smart4aviation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smart4aviationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Smart4aviationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/drzymala/smart4aviation/Smart4aviationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drzymala.smart4aviation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smart4aviationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
