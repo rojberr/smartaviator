3fb9fb83bad911f126b40e5e4568b947d878981c
Rojberr <rojberr@outlook.com>
2022-05-04 20:15:43 +0200

    feat(web): Use ResponseEntity<> in AviationController
    * Return notFound() on failure
    * Return data and ok on success
    
    Relates: #NO-TICKET

--- src/main/java/dev/drzymala/smart4aviation/cargo/web/SmartAviationController.java
+++ src/main/java/dev/drzymala/smart4aviation/cargo/web/SmartAviationController.java
@@ -2,13 +2,12 @@ package dev.drzymala.smart4aviation.cargo.web;
 
 import dev.drzymala.smart4aviation.cargo.application.port.CargoInitializerUseCase;
 import dev.drzymala.smart4aviation.cargo.application.port.CargoUseCase;
-import dev.drzymala.smart4aviation.cargo.domain.Flight;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
-import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -18,18 +17,24 @@ public class SmartAviationController {
     private final CargoInitializerUseCase initializer;
 
     @GetMapping("/cargo-weight")
-    @ResponseStatus(HttpStatus.OK)
-    public Optional<Flight> getWeight(@RequestParam Long flightId, @RequestParam Instant date) {
-        return cargoService.getWeight(flightId, date);
+    public ResponseEntity<?> getWeight(@RequestParam Long flightId, @RequestParam Instant date) {
+        return cargoService
+                .getWeight(flightId, date)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/cargo-amount")
-    @ResponseStatus(HttpStatus.OK)
-    public Optional<Flight> getFlightsAndBaggageAmount(@RequestParam String iata, @RequestParam Instant date) {
-        return cargoService.getFlightsAndBaggageAmount(iata, date);
+    public ResponseEntity<?> getFlightsAndBaggageAmount(@RequestParam String iata, @RequestParam Instant date) {
+        return cargoService
+                .getFlightsAndBaggageAmount(iata, date)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping("/init")
     @ResponseStatus(HttpStatus.CREATED)
-    public void initialize() { initializer.initialize(); }
+    public void initialize() {
+        initializer.initialize();
+    }
 }
