9f4b9ad0ba54bc7bea6d65a7f50cd55e99bb95ea
rojberr <rojberr@outlook.com>
2022-07-19 16:38:35 +0200

    web(init): Add HATEOAS link in /init response

--- src/main/java/dev/drzymala/smart4aviation/cargo/web/TestDataInitController.java
+++ src/main/java/dev/drzymala/smart4aviation/cargo/web/TestDataInitController.java
@@ -1,12 +1,20 @@
 package dev.drzymala.smart4aviation.cargo.web;
 
 import dev.drzymala.smart4aviation.cargo.application.port.CargoInitializerUseCase;
+import dev.drzymala.smart4aviation.cargo.application.port.CargoUseCase;
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @AllArgsConstructor
@@ -14,10 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestDataInitController {
 
     private final CargoInitializerUseCase initializer;
+    private final CargoUseCase cargoService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void initialize() {
+    public EntityModel initialize() {
+
         initializer.initialize();
+        Link cargoLinkFlightDate = linkTo(SmartAviationController.class)
+                .slash(cargoService.getWeight(2741L, Instant.parse("2018-03-31T06:52:28.00Z")))
+                .withSelfRel();
+
+        return EntityModel.of(cargoLinkFlightDate);
     }
 }
